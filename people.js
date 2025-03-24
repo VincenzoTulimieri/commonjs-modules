0b904bcf2c9f281356ec1879f86574d2918efac1
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-24 18:22:48 +0100

    feat: creata funzione con fullName e hobbies

--- people.js
+++ people.js
@@ -2,3 +2,12 @@ const myName = require('./name.js')
 const myHobbies = require('./hobbies.js')
 console.log(myName)
 console.log(myHobbies)
+
+function  myProfile(){
+    return {
+        fullName: myName,
+        hobbies: myHobbies
+    }
+}
+
+console.log(myProfile())
